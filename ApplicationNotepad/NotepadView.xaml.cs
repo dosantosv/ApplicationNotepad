d81b7f014b9f641595f0537745128ef32f26ed97
Vinícius Santos <102265413+dosantosv@users.noreply.github.com>
2024-10-02 21:23:58 -0300

    create application notepad

---
+++ ApplicationNotepad/NotepadView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+namespace ApplicationNotepad
+{
+    /// <summary>
+    /// Interaction logic for NotepadView.xaml
+    /// </summary>
+    public partial class NotepadView : Window
+    {
+        public NotepadView()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new()
+            {
+                Filter = GetOpenFileDialogFilter()
+            };
+
+            if (openFileDialog.ShowDialog() is true)
+                txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new()
+            {
+                Filter = GetOpenFileDialogFilter()
+            };
+
+            if (saveFileDialog.ShowDialog() is true)
+                File.WriteAllText(saveFileDialog.FileName, txtEditor.Text);
+        }
+
+        private string GetOpenFileDialogFilter()
+        {
+            return "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+        }
+    }
+}
